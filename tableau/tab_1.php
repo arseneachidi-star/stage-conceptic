76b20d0d2a75b7d38b10bb9ea095a4c35337590b
Arsène Atchidi <arseneachidi@gmail.com>
2026-07-24 10:51:23 +0200

    Tous les fichiers tableau sont ici

---
+++ tableau/tab_1.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = ["Banane" => 1000, "Orange" => 3000, " Mangue" => 5000];
+foreach( $fruits as $nom => $valeur )
+    echo " Le prix de $nom est disponible à $valeur FCFA </br>";
+
+?>
